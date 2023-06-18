2ee74bc53aa18e2dd1866668a73fbd78761d4360
Татьяна <tanechka201286grudeva@gmail.com>
2023-06-18 19:36:19 +0300

    doing exercise

---
+++ catalog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name},{phone},{message}')
+    return render(request, 'catalog/index.html')
+
+
+
