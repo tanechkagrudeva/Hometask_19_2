194e8a76daa8bd1ad79e96955b9068c8da9783ae
Татьяна <tanechka201286grudeva@gmail.com>
2023-06-21 21:24:49 +0300

    add contact.html

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from catalog.views import index
+from catalog.views import index, contact
 
 urlpatterns = [
     path('', index),
+    path('contact/', contact),
 ]
 
--- catalog/views.py
+++ catalog/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 
 
 def index(request):
+    return render(request, 'catalog/index.html')
+
+
+def contact(request):
     if request.method == 'POST':
         name = request.POST.get('name')
-        phone = request.POST.get('phone')
+        email = request.POST.get('email')
         message = request.POST.get('message')
-        print(f'{name},{phone},{message}')
-    return render(request, 'catalog/index.html')
+        print(f'{name}\n{email}\n{message}')
+
+    return render(request, 'catalog/contact.html')
 
 
 
