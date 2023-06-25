1e7178559f10efed3e574d7a3801802496b779b4
Татьяна <tanechka201286grudeva@gmail.com>
2023-06-25 17:38:22 +0300

    add two models for catalog

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    preview = models.ImageField(upload_to='previews/', verbose_name='Превью', **NULLABLE)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+    price = models.DecimalField(max_digits=5 , decimal_places=2, ** NULLABLE, verbose_name='Цена')
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=False, **NULLABLE, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=False, auto_now_add=False, **NULLABLE, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name}, {self.category}'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ('name',)
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+         verbose_name = 'Категория'
+         verbose_name_plural = 'Категории'
+         ordering = ('name',)
+
--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project',
+        'USER': 'postgres',
+        'PASSWORD': 'Zima-2023'
     }
 }
 
@@ -123,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
